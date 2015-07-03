34958a5cfd4d2c8c4b80736092978d2cf61fe89d
phase <ph4sesaber@gmail.com>
2015-07-03 13:02:16 -0700

    Create birthday.fish

---
+++ birthday.fish
@@ -0,0 +1,11 @@
+v
+\0&
+v                                   <
+\&:&2(?v
+       >"Happy Birthday to you"a&1+&^
+\"Happy Birthday dear ><>"a\
+v  a"uoy ot yadhtriB yppaH"/
+r
+>l?!;o
+
+For http://codegolf.stackexchange.com/questions/39752/sing-happy-birthday-to-your-favourite-programming-language?page=3&tab=votes#tab-top
