37a8ae7d5d5790b4da407547db1e7ae52710bd5d
phase <ph4sesaber@gmail.com>
2015-11-04 16:14:23 -0800

    Some idea for http://codegolf.stackexchange.com/questions/62960

---
+++ U.java
@@ -0,0 +1,22 @@
+//some idea for http://codegolf.stackexchange.com/questions/62960
+import java.util.*;class U {
+    ArrayList<P> p=new ArrayList<P>();
+    Random r=new Random();
+    public static void main(String[]a){
+        p.add(new H()); //make configurable
+        for(int c=0;;c++){
+            int j=0;
+            for(P s:p){
+                if(s.d.isEmpty()) j++
+                if(p instanceof H){
+                    
+                }
+            }
+            if(j==p.size()){System.out.println("Simulation ended after 0."+c+" yoctoseconds.");break;}
+            Thread.sleep(.00000000000000000000016);
+        }
+    }
+    class P{public String n;public HashMap<P[],Float> d = new HashMap<P[],Float>();}
+    class H extends P{{this.n = "Higgs boson";d.put({new BQ(),new BAQ()}, 64.8);d.put()}}
+    
+}
